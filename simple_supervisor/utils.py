91df811204db7d51c1f2eef1b16d71c9c3fa06db
Carl Harris <elgoogemail2007@gmail.com>
2022-05-07 03:37:06 -0400

    allow more freedom for key names

--- simple_supervisor/utils.py
+++ simple_supervisor/utils.py
@@ -1,12 +1,15 @@
 import os
+import re
 
 from . import exceptions
+
+endswithnumber = re.compile('\d+$').search
 
 def is_numbered_key(key, prefix):
     """
     String `key` is prefixed by `prefix` and immediately followed by a number.
     """
-    return key.startswith(prefix) and key[len(prefix):].isdigit()
+    return key.startswith(prefix) and endswithnumber(key)
 
 def raise_for_missing(paths):
     """
