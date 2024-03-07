f59a29f7d0a800a03b983061680a8c6c58c9438f
Carl Harris <hitbox@ryzen.com>
2024-03-06 23:12:38 -0500

    logging
    - added logging back
    - break functions up

--- update_repos.py
+++ update_repos.py
@@ -5,7 +5,7 @@ import logging.config
 import os
 import subprocess
 
-APPNAME = 'simple_supervisor'
+APPNAME = 'update_repos'
 
 def onerror(os_error):
     raise os_error
@@ -42,39 +42,23 @@ def get_branch_name(root):
     branch = ref.split('/')[-1]
     return branch
 
-def git_fetch_and_hard_reset_origin_branch(top, exclude):
+def git_fetch_and_hard_reset_origin_branch(root):
     options = dict(
         capture_output = True,
         check = True,
         text = True,
     )
-    # XXX: WIP
-    # - the git commands are changing the state of the filesystem
-    # - so we capture in a list
-    # - would rather generate
-    roots = list(dir_with_git(top))
-    for root in roots:
-        if root in exclude:
-            print(f'excluding: {root}')
-            continue
-        else:
-            print(root)
-
-        # XXX: WIP
-        if not os.path.exists(root):
-            continue
-
-        options.update(cwd=root)
-        command = ['git', 'fetch']
+    options.update(cwd=root)
+    command = ['git', 'fetch']
+    result = subprocess.run(command, **options)
+    if result.stdout or result.stderr:
+        # guessing that if there was standard output, then we proceed
+        yield result
+        branch = get_branch_name(root)
+        command = ['git', 'reset', '--hard', f'origin/{branch}']
         result = subprocess.run(command, **options)
         if result.stdout or result.stderr:
-            # guessing that if there was standard output, then we proceed
-            print_result(result)
-            branch = get_branch_name(root)
-            command = ['git', 'reset', '--hard', f'origin/{branch}']
-            result = subprocess.run(command, **options)
-            if result.stdout:
-                print_result(result)
+            yield result
 
 def main(argv=None):
     """
@@ -107,13 +91,36 @@ def main(argv=None):
     else:
         top = app_section['top']
 
+    logger = logging.getLogger(APPNAME)
+
     start_time = datetime.datetime.now()
-    git_fetch_and_hard_reset_origin_branch(top, exclude)
+
+    # XXX: WIP
+    # - the git commands are changing the state of the filesystem
+    # - so we capture in a list
+    # - would rather generate
+    logger.debug('finding .git roots')
+    roots = list(dir_with_git(top))
+
+    for root in roots:
+        if root in exclude:
+            logger.debug(f'excluding: {root}')
+            continue
+        else:
+            logger.debug(f'update: {root}')
+
+        for result in git_fetch_and_hard_reset_origin_branch(root):
+            for attr in ['stdout', 'stderr']:
+                output = getattr(result, attr)
+                for line in output.splitlines():
+                    logger.debug('%s: %s', attr, line)
+
     end_time = datetime.datetime.now()
     elapsed = end_time - start_time
-    print(f'{start_time=}')
-    print(f'{elapsed=}')
-    print(f'{end_time=}')
+
+    logger.debug('start_time=%s', start_time)
+    logger.debug('elapsed=%s', elapsed)
+    logger.debug('end_time=%s', end_time)
 
 if __name__ == '__main__':
     main()
